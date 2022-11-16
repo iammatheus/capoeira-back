f531386712241de956bc11809db111be2256ab03
Matheus Ferreira <matheus.play.baixista@hotmail.com>
2022-11-16 16:40:27 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const path = require("path");
 const routes = require("./routes");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 module.exports = app;
