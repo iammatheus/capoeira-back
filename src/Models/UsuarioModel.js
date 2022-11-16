b1496eaaef9587147fd17536ff072d65b3926215
Matheus Ferreira Siqueira <matheusferreirasiqueira9@gmail.com>
2022-11-16 18:55:44 -0300

    adicionando paginação de eventos

---
+++ src/Models/UsuarioModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const UsuarioSchema = new Schema({
+  id: ObjectId,
+  nome: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  senha: {
+    type: String,
+    required: true,
+  },
+  imagemUrl: {
+    type: String,
+    required: false,
+  },
+});
+
+const UsuarioModel = mongoose.model("usuarios", UsuarioSchema);
+module.exports = UsuarioModel;
